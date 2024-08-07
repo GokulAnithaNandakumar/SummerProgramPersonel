657e21848cc1e9dad77f6681d95e739431579697
Goutham Kurapati <gouthamkurapati@gmail.com>
2024-08-07 22:41:22 +0530

    Fixed community

--- src/components/Community/index.jsx
+++ src/components/Community/index.jsx
@@ -6,30 +6,30 @@ import { Link } from "react-router-dom";
 const Community = () => {
   return (
     <div className='community photo-section'>
-        <div className='max-width'>
-          <div className='photo-section-child'>
-            <div className='photo-section-top'> 
-              <div className='photo-section-heading'>
-                Community Impact
-              </div>
-              <div className='photo-section-subheading'>
-                
-              </div>
+      <div className='max-width'>
+        <div className='photo-section-child'>
+          <div className='photo-section-top'>
+            <div className='photo-section-heading'>
+              Community Impact
             </div>
-            <div className='photo-section-bottom'> 
-              <div className='photo-section-description'>
-                Azero Tech is more than just a tech company; we are a community-driven initiative. We
-                provide valuable opportunities for college students to gain practical experience, develop
-                their skills, and earn while they learn. By fostering a collaborative environment, we aim to
-                empower the next generation of tech innovators.
+            <div className='photo-section-subheading'>
 
-              </div>
-              <div>
-                <Link to="/about"><Button buttonText='Know More' /></Link>
-              </div>
+            </div>
+          </div>
+          <div className='photo-section-bottom'>
+            <div className='photo-section-description'>
+              The IETVIT Summer Program empowers youth to solve real-world problems, 
+              fostering critical thinking and innovation. Through hands-on projects in healthcare, 
+              environment, and technology, participants develop practical skills while addressing 
+              community challenges. This initiative promotes collaboration, raises awareness, and 
+              inspires future programs, creating lasting positive impacts and strengthening community ties.
+            </div>
+            <div>
+              <Link to="/projects"><Button buttonText='Know More' /></Link>
             </div>
           </div>
         </div>
+      </div>
     </div>
   )
 }
